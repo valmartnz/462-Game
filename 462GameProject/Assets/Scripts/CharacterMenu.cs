d55af073040f40320df8b3e9baf032971210b457
valmartnz <47126912+valmartnz@users.noreply.github.com>
2022-04-28 23:25:52 -0700

    Added play button to go to tutorial scene

---
+++ 462GameProject/Assets/Scripts/CharacterMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterMenu : MonoBehaviour
+{
+    public GameObject character;
+    public void Submit() {
+        PrefabUtility.SaveAsPrefabAsset(character, "Assets/Prefabs/Player.prefab");
+        SceneManager.LoadScene(1);
+    }
+}
