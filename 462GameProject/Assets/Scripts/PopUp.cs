97f97f32f427cf4f7e8db2e5e40608cde50fe004
valmartnz <47126912+valmartnz@users.noreply.github.com>
2022-05-06 19:33:38 -0700

    Added popUp to show what key to press

---
+++ 462GameProject/Assets/Scripts/PopUp.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PopUp : MonoBehaviour
+{
+    public bool playerInRange;
+    // public KeyCode interactKey;
+    // public UnityEvent interactAction;
+    public GameObject popUpBox;
+    // public GameObject player;
+    public bool show_popUp;
+
+    public void OpenPopUp() {
+        show_popUp = true;
+        popUpBox.SetActive(show_popUp);
+        Debug.Log("Popup is open");
+    }
+
+    public void ClosePopUp() {
+        show_popUp = false;
+        popUpBox.SetActive(show_popUp);
+        Debug.Log("Popup is closed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange)
+        {
+            OpenPopUp();
+        } else {
+            ClosePopUp();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Fighter"))
+        {
+            playerInRange = true;
+            Debug.Log("Player can see popup");
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.CompareTag("Fighter"))
+        {
+            playerInRange = false;
+            Debug.Log("Player cannot see popup");
+        }
+    }
+}
